0dca81faf73b758ed07ff759749ca5b494d5d2cb
baitcode <batiyiv@gmail.com>
2025-02-26 21:06:16 +0100

    Added precommit hooks for linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
 import * as React from 'react';
 import './index.css';
 import './App.css';
-import { TonConnectUIProvider } from './ton-ui';
+import { TonConnectUIProvider, TonConnectButton } from './ton-ui';
 
 const manifest = new URL('ton-connect-manifest.json', import.meta.url);
-const logo = new URL('logo.svg', import.meta.url);
+// const logo = new URL('logo.svg', import.meta.url);
+
 
 function App() {
   return (
     <TonConnectUIProvider manifestUrl={manifest.href}>
       <div className="App">
         <header className="App-header">
-          <img src={logo.pathname} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <span>My App with React UI</span>
+          <TonConnectButton />
         </header>
       </div>
     </TonConnectUIProvider>
