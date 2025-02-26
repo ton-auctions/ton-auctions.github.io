619efbaddf3e71b79037b7e022ea63de4a24516a
baitcode <batiyiv@gmail.com>
2025-02-26 21:56:21 +0100

    remove unnecessary css add parcel config not to convert ton manifest to js

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
 import './index.css';
-import './App.css';
 import { TonConnectUIProvider, TonConnectButton } from './ton-ui';
 
-const manifest = new URL('ton-connect-manifest.json', import.meta.url);
+const manifest = new URL('./ton-connect-manifest.json', import.meta.url);
 
 function App() {
   return (
     <TonConnectUIProvider manifestUrl={manifest.href}>
-      <div className="App">
-        <header className="App-header">
+      <div>
+        <header className='flex flex-col items-center justify-center min-h-screen bg-gray-700 text-white'>
           <span>My App with React UI</span>
-          <span>{manifest.href}</span>
           <TonConnectButton />
         </header>
       </div>
