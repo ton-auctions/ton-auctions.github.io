99409b2e726291c4fd720902dd27d8e2976b1d6b
baitcode <batiyiv@gmail.com>
2025-02-26 21:38:17 +0100

    test manifest

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import { TonConnectUIProvider, TonConnectButton } from './ton-ui';
 
 const manifest = new URL('ton-connect-manifest.json', import.meta.url);
 
-
 function App() {
   return (
     <TonConnectUIProvider manifestUrl={manifest.href}>
       <div className="App">
         <header className="App-header">
           <span>My App with React UI</span>
+          <span>{manifest.href}</span>
           <TonConnectButton />
         </header>
       </div>
