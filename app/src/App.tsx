c4f98b90efef6add1ec50ff6f8f727d19d7caae2
baitcode <batiyiv@gmail.com>
2025-03-28 23:03:00 +0300

    Parcel build problem

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,23 +1,18 @@
 import * as React from "react";
 import "./index.css";
-import {
-  TonConnectUIProvider,
-  TonConnectButton,
-  useTonWallet,
-} from "@tonconnect/ui-react";
+import { TonConnectUIProvider, TonConnectButton } from "@tonconnect/ui-react";
 import { useEffect } from "react";
-import { WebApp } from "@grammyjs/web-app";
 
 const manifest = new URL("./ton-connect-manifest.json", import.meta.url);
 
-const CONTROLLER_ADDRESS = "EQAs3c0OAgJHg4Jq__YwHEIaqM8MmcGzjzzksKGz0zEBJCN6";
+// const CONTROLLER_ADDRESS = "EQAs3c0OAgJHg4Jq__YwHEIaqM8MmcGzjzzksKGz0zEBJCN6";
 
 function App() {
   // const wallet = useTonWallet();
 
   useEffect(() => {
-    console.log(WebApp.initDataUnsafe.chat?.id);
-    console.log(WebApp.initDataUnsafe.chat?.username);
+    // console.log(WebApp.initDataUnsafe.chat?.id);
+    // console.log(WebApp.initDataUnsafe.chat?.username);
   }, []);
 
   return (
@@ -25,7 +20,7 @@ function App() {
       <div>
         <header className="flex flex-col items-center justify-center min-h-screen bg-gray-700 text-white">
           <span>My App with React UI</span>
-          <span>{JSON.stringify(WebApp.initDataUnsafe)}</span>
+          {/* <span>{JSON.stringify(WebApp.initDataUnsafe)}</span> */}
           <TonConnectButton />
         </header>
       </div>
