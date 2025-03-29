ad16b9eac0f7d883a69c810d7695a660cdb85c80
baitcode <batiyiv@gmail.com>
2025-03-29 03:11:52 +0300

    dancing around with manifest file

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,28 +1,35 @@
 import * as React from "react";
 import "./index.css";
-import { TonConnectUIProvider, TonConnectButton } from "@tonconnect/ui-react";
+import {
+  TonConnectUIProvider,
+  TonConnectButton,
+  useTonWallet,
+} from "@tonconnect/ui-react";
 import { useEffect } from "react";
-
-const manifest = new URL("./ton-connect-manifest.json", import.meta.url);
 
 // const CONTROLLER_ADDRESS = "EQAs3c0OAgJHg4Jq__YwHEIaqM8MmcGzjzzksKGz0zEBJCN6";
 
-function App() {
-  // const wallet = useTonWallet();
+function View() {
+  const wallet = useTonWallet();
 
-  useEffect(() => {
-    // console.log(WebApp.initDataUnsafe.chat?.id);
-    // console.log(WebApp.initDataUnsafe.chat?.username);
-  }, []);
+  // {/* <span>{JSON.stringify(WebApp.initDataUnsafe)}</span> */}
+  return (
+    <React.Fragment>
+      <span>My App with React UI</span>
+      {wallet?.account.address}
+    </React.Fragment>
+  );
+}
+
+function App() {
+  useEffect(() => {}, []);
 
   return (
-    <TonConnectUIProvider manifestUrl={manifest.href}>
+    <TonConnectUIProvider
+      manifestUrl={"https://ton-auctions.github.io/ton-connect-manifest.json"}
+    >
       <div>
-        <header className="flex flex-col items-center justify-center min-h-screen bg-gray-700 text-white">
-          <span>My App with React UI</span>
-          {/* <span>{JSON.stringify(WebApp.initDataUnsafe)}</span> */}
-          <TonConnectButton />
-        </header>
+        <TonConnectButton />
       </div>
     </TonConnectUIProvider>
   );
